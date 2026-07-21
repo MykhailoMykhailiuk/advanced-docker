837ae2e499643888fdc79016202bc61379ffccf0
MykhailoMykhailiuk <mykhailo.mykhailiuk.prof@gmail.com>
2026-07-21 14:32:10 +0100

    v1

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello Docker")
+# app.py
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"hello docker")
+
+HTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
